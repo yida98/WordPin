d95c44a7d729b87bc2b115544e9e7490bb3b7a73
Yida Zhang <carriedada@gmail.com>
2023-07-13 00:09:03 -0700

    wdp://18 Setup Color Assets

---
+++ WordPin/WordPin/View/Helper/Color_ext.swift
@@ -0,0 +1,25 @@
+//
+//  Color_ext.swift
+//  WordPin
+//
+//  Created by Yida Zhang on 7/12/23.
+//
+
+import Foundation
+
+import SwiftUI
+
+extension Color {
+    // MARK: - Environment
+    static let background: Color = Color("background")
+    
+    // MARK: - LCD Display
+    static let screenBackground: Color = Color("screenBackground")
+    static let primaryFont: Color = Color("primaryFont")
+    static let secondaryFont: Color = Color("secondaryFont")
+    static let tertiaryFont: Color = Color("tertiaryFont")
+    
+    // MARK: - Light
+    static let success: Color = Color("success")
+    static let failure: Color = Color("failure")
+}
