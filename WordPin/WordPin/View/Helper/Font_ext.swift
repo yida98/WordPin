b61f818fbf7bb9dbcc48953f115372c69070a622
Yida Zhang <carriedada@gmail.com>
2023-07-12 23:46:17 -0700

    wdp://17 Font Design

---
+++ WordPin/WordPin/View/Helper/Font_ext.swift
@@ -0,0 +1,47 @@
+//
+//  Font_ext.swift
+//  WordPin
+//
+//  Created by Yida Zhang on 7/12/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    public static func gameFont(size: UIFont.TextStyle = .body) -> Font {
+        Font.custom("PublicPixel-z84yD", size: UIFont.preferredFont(forTextStyle: size).pointSize)
+    }
+    
+    public static func digitalFont(size: UIFont.TextStyle = .body) -> Font {
+        Font.custom("DigitalNumbers-Regular", size: UIFont.preferredFont(forTextStyle: size).pointSize)
+    }
+    
+    public static func secondaryFont(size: UIFont.TextStyle = .body) -> Font {
+        Font.custom("VT323-Regular", size: UIFont.preferredFont(forTextStyle: size).pointSize)
+    }
+    
+    public static func primary(size: CGFloat, emphasis: Emphasis = .regular) -> Font {
+        var font = "IBMPlexSans-Regular"
+        switch emphasis {
+        case .semiBoldItalic: font = "IBMPlexSans-SemiBoldItalic"
+        case .medium: font = "IBMPlexSans-Medium"
+        case .semiBold: font = "IBMPlexSans-SemiBold"
+        case .mediumItalic: font = "IBMPlexSans-MediumItalic"
+        case .lightItalic: font = "IBMPlexSans-LightItalic"
+        case .extraLight: font = "IBMPlexSans-ExtraLight"
+        case .thin: font = "IBMPlexSans-Thin"
+        case .thinItalic: font = "IBMPlexSans-ThinItalic"
+        case .boldItalic: font = "IBMPlexSans-BoldItalic"
+        case .light: font = "IBMPlexSans-Light"
+        case .italic: font = "IBMPlexSans-Italic"
+        case .bold: font = "IBMPlexSans-Bold"
+        default: break
+        }
+        return Font.custom(font, size: size)
+    }
+    
+    public enum Emphasis {
+        case semiBoldItalic, medium, semiBold, mediumItalic, lightItalic, extraLight, thin, thinItalic, boldItalic, light, italic, regular, extraLightItalic, bold
+    }
+}
